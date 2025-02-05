e6a1e1ead6dab4198465707e0022f84ce6c72ed6
Rohit Chauhan <rohit.911.chauhan@gmail.com>
2025-02-05 15:01:19 +0530

    Update utils.go
    Signed-off-by: Rohit Chauhan <rohit.911.chauhan@gmail.com>

--- medkg-api/neo4j/utils.go
+++ medkg-api/neo4j/utils.go
@@ -59,31 +59,6 @@ func checkForSource(nodeProps map[string]interface{}, label string) (string, str
 		return val1.(string), ""
 	}
 
-	// Now check for description with any url
-	if val, ok := nodeProps["description"]; ok {
-		if val.(string) != "" {
-			// Check if the description has a url, it'll be of type this [url:http\://example.com], but it can have more text before or after this
-			// So we need to extract the url from this
-			// First we need to find the index of the url
-			startIndex := strings.Index(val.(string), "[url:")
-			if startIndex != -1 {
-				// Now we need to find the index of the closing bracket
-				endIndex := strings.Index(val.(string), "]")
-				if endIndex != -1 {
-					// Now we need to extract the url from the description
-					url := val.(string)[startIndex+5 : endIndex]
-					//now check for the host in this url, that'll be our source
-					if strings.Contains(url, "http") {
-						temp := strings.Split(url, "/")[2]
-						temp = strings.ReplaceAll(temp, "www.", "")
-						temp = strings.Split(temp, ".")[0]
-						return temp, url
-					}
-				}
-			}
-		}
-	}
-
 	switch label {
 	case "Gene":
 		return "GeneCards", "https://www.genecards.org/"
@@ -113,4 +88,58 @@ func checkForSource(nodeProps map[string]interface{}, label string) (string, str
 		return "PubMed", "https://pubmed.ncbi.nlm.nih.gov/"
 	}
 	return "", ""
+	// // Now check for description with any url
+	// if val, ok := nodeProps["description"]; ok {
+	// 	if val.(string) != "" {
+	// 		// Check if the description has a url, it'll be of type this [url:http\://example.com], but it can have more text before or after this
+	// 		// So we need to extract the url from this
+	// 		// First we need to find the index of the url
+	// 		startIndex := strings.Index(val.(string), "[url:")
+	// 		if startIndex != -1 {
+	// 			// Now we need to find the index of the closing bracket
+	// 			endIndex := strings.Index(val.(string), "]")
+	// 			if endIndex != -1 {
+	// 				// Now we need to extract the url from the description
+	// 				url := val.(string)[startIndex+5 : endIndex]
+	// 				//now check for the host in this url, that'll be our source
+	// 				if strings.Contains(url, "http") {
+	// 					temp := strings.Split(url, "/")[2]
+	// 					temp = strings.ReplaceAll(temp, "www.", "")
+	// 					temp = strings.Split(temp, ".")[0]
+	// 					return temp, url
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	// switch label {
+	// case "Gene":
+	// 	return "GeneCards", "https://www.genecards.org/"
+	// case "Disease":
+	// 	return "Disease Ontology", "http://disease-ontology.org/"
+	// case "Drug":
+	// 	return "DrugBank", "https://www.drugbank.ca/"
+	// case "Pathway":
+	// 	return "Reactome", "https://reactome.org/"
+	// case "Protein":
+	// 	return "UniProt", "https://www.uniprot.org/uniprotkb/" + nodeProps["id"].(string)
+	// case "Compound":
+	// 	return "PubChem", "https://pubchem.ncbi.nlm.nih.gov/"
+	// case "Anatomy":
+	// 	return "Uberon", "http://uberon.github.io/"
+	// case "Cell":
+	// 	return "CL", "http://www.obofoundry.org/ontology/cl.html"
+	// case "Biological Process":
+	// 	return "GO", "http://geneontology.org/"
+	// case "Molecular Function":
+	// 	return "GO", "http://geneontology.org/"
+	// case "Cellular Component":
+	// 	return "GO", "http://geneontology.org/"
+	// case "Phenotype":
+	// 	return "HPO", "https://hpo.jax.org/"
+	// case "Publication":
+	// 	return "PubMed", "https://pubmed.ncbi.nlm.nih.gov/"
+	// }
+	// return "", ""
 }
